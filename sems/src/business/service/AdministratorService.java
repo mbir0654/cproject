de58395d08eee01eb558a6628112e4b759c5a4e7
myh <mihutbogdan@gmail.com>
2011-03-24 23:06:50 +0200

    test cu EGIT

--- sems/src/business/service/AdministratorService.java
+++ sems/src/business/service/AdministratorService.java
@@ -3,10 +3,18 @@
  */
 package business.service;
 
+import business.model.*;
+
 /**
  * @author myh
  * 	
  */
 final class AdministratorService {
+	public void addStudent(Student s){
+		/**TODO
+		 * deschide fereastra adugare student
+		 * adaugare student in repository de studenti
+		 */
+	}
 
 }
