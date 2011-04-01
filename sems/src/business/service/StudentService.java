e24e521546e37223a914afd294fd9818da841b3e
bogdan <bogdan@123linkit.com>
2011-04-01 09:09:59 +0300

    Business service -> student

--- sems/src/business/service/StudentService.java
+++ sems/src/business/service/StudentService.java
@@ -6,6 +6,8 @@ package business.service;
 import java.awt.List;
 import java.util.ArrayList;
 
+import javax.swing.JList;
+
 import business.model.*;
 
 /**
@@ -17,31 +19,50 @@ final class StudentService {
 	 * Studentul pentru care efectuam serviciile
 	 */
 	private Student s;
-	public void viewCourses(Student s){
+	public void viewCourses(){
 		/**TODO
 		 * incarca intr-un jlist disciplinele la care e inscris studentul 
 		 */
 	}
-	public void exitSystem(Student s){
+	public void exitSystem(){
 		/**TODO
 		 * logout user salveaza ce trebuie salvat in baza de date
 		 */
 	}
-	public void viewAnnouncements(Student s){
+	public void viewAnnouncements(){
 		/**TODO
 		 * vizualizare anunturi pentru student s 
 		 */
 	}
-	public void makeContract(Student s){
+	public void makeContract(){
 		/**TODO
 		 * load courses
 		 * open makeContractDialog
 		 */
 	}
-	public void viewAvailableCourses(Student s){
+	public void loadAvailableCourses(JList list){
 		/**TODO
 		 * load available courses from db and reload a jlist  
 		 */
 	}
-	
+	public void loadCourseMaterials(JList list){
+		/**TODO
+		 * load materials from db and reload a jlist  
+		 */
+	}
+	public void downloadCourseMaterial(/*CourseMaterial*/){
+		/**TODO
+		 * doanload selected course materials   
+		 */
+	}
+	public void sendHomeWork(Disciplina d){
+		/**TODO
+		 * upload homework for course   
+		 */
+	}
+	public void viewGrades(JList list){
+		/**TODO
+		 * load available grades from db and reload list  
+		 */
+	}
 }
