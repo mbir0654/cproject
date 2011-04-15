ab817260100d9f385b8751436d86b3e5940c67db
Otniel Nicola <Otniel.Nicola@gmail.com>
2011-04-15 22:14:26 +0300

    ui 2

---
+++ sems/src/test/GuiTester.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package test;
+
+import javax.swing.*;
+/*import java.awt.*;
+import java.awt.event.*;*/
+import ui.*;
+
+/**
+ * @author otniel
+ *
+ */
+
+public class GuiTester {
+	
+	/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame loginFrame = new frameLogin();
+        loginFrame.setVisible(true);
+        loginFrame.setTitle("SEMS:: Login");
+        loginFrame.setName("Login");
+        loginFrame.setResizable(false);
+    }
+}
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
