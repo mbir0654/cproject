0c5f2feb9a41ce1c669abd31f512e25a8d2e5191
Adrian Bunta <adi_bunta@yahoo.com>
2011-05-28 16:16:34 +0300

    Modificat UI Admin

--- sems_client/src/client/SemsClient.java
+++ sems_client/src/client/SemsClient.java
@@ -14,7 +14,7 @@ public class SemsClient {
     public static void main(String[] args) {
    try
     {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
     }
     catch (Exception e)
     {    }
