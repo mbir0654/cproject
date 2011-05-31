8a8a0922e1c6a9a9d5e5c00396809d36cc27d7e1
Otniel Nicola <Otniel.Nicola@gmail.com>
2011-05-31 22:08:11 +0300

    cleared tests

--- sems_server/src/server/SemsServer.java
+++ sems_server/src/server/SemsServer.java
@@ -1,19 +1,5 @@
 package server;
 
-import business.model.*;
-import data.dbutil.DbUtil;
-import data.repositorydb.*;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -24,35 +10,5 @@ public class SemsServer {
     public static void main(String args[]) {
         ApplicationContext factory = new ClassPathXmlApplicationContext(
                 "classpath:server/serverContext.xml");
-        try {
-            DbUtil dbu = new DbUtil();
-            ResultSet rs1 = dbu.getDate("select * from courses");
-            while(rs1.next())
-            System.out.println(rs1.getString(2));
-        } catch (SQLException ex) {
-            Logger.getLogger(SemsServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        Course c = new Course("Ingineria SistemelorSoft", 6, null);
-        c.setSpecializare(SpecialityRepository.getInstance().findByName("informatica"));
-        c.generateCod();
-        for (Professor p : ProfessorRepository.getInstance().getAll()){
-            c.addProfessor(p);
-        }
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        Announcement announce = new Announcement("text anunt", "titlu anunt nou", new Date(), c);
-        announce.setProf(ProfessorRepository.getInstance().findByName("bobby"));
-        c.addAnnouncement(announce);
-        try {
-            c.addAssignment(new Assignment("titlu tema noua", "cerinta temei", sdf.parse("08-06-2011"), c));
-            c.addExam(new Exam(sdf.parse("08-06-2011"), "partial", c));
-        } catch (ParseException ex) {
-            Logger.getLogger(SemsServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        CourseMaterial nmc = new CourseMaterial();
-        nmc.setPath("~/sems/cproject/sems_server/doc/Documentatie\\ PC/221-II (1).docx");
-        c.addMaterialCurs(nmc);
-        System.out.println(c.getName());
-        c.setTip("OPTIONAL");
-        CourseRepository.getInstance().delete(c);
     }
 }
